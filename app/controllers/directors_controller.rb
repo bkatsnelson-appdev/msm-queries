d5c73f1a2b2541b63253024b16831efd0aa37e51
beatricek222 <beatricek222@gmail.com>
2022-10-26 19:51:35 +0000

    Directors table

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,11 @@
 class DirectorsController < ApplicationController
 
   def index
+    @list_of_directors = Director.all
     render({ :template => "directors_template/index.html.erb"})
+  end
+
+  def wisest
+    render({ :template => })
   end
 end
